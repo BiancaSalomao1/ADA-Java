e454a09d39d911c9a22a5ba1a085a7904d5b439d
BiancaSalomao1 <biancasalomao2024@gmail.com>
2026-05-04 11:26:00 -0300

    Estudos Programacao Funcional

---
+++ src/Aula29Abr2026/src/java/Atleta.java
@@ -0,0 +1,12 @@
+package Aula29Abr2026.src.java;
+
+class Atleta {
+
+    String nome;
+    String nacionalidade;
+    double altura, peso;
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ src/Aula29Abr2026/src/java/Modalidade.java
@@ -0,0 +1,8 @@
+package Aula29Abr2026.src.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+enum Modalidade{
+    CRAWL, PEITO, COSTAS, BORBOLETA;
+}
